74acc4242fd98cc6a04bb960981cd91b7926bc53
Karthik Sreenivasan <101344162+karthiksreenivasanwork@users.noreply.github.com>
2022-06-04 12:40:43 +0530

    Completed the following --------------------------
    1. IPlatformRepo: This interface defines what members will be a part of the repository concrete class.
    2. PlatformRepo: Implements IPlatformRepo.

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,45 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        /// <summary>
+        /// Dependency injection of AppDbContext reference
+        /// </summary>
+        /// <param name="context">PlatformService.Data.AppDbContext</param>
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreatePlatform(Platform plat)
+        {
+            if (plat == null)
+            {
+                throw new ArgumentNullException(nameof(plat));
+            }
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.PlatForms.ToList();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            return _context.PlatForms.FirstOrDefault(p => p.Id == id);
+        }
+
+        /// <summary>
+        /// Any CRUD unsaved operation from the datasource will be flushed or saved to the database. 
+        /// </summary>
+        /// <returns>True if the changes were saved and false otherwise.</returns>
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}

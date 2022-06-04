01d82ab67c6f35f3b95d28848e39c1e63684c09e
Karthik Sreenivasan <101344162+karthiksreenivasanwork@users.noreply.github.com>
2022-06-04 11:43:22 +0530

    Completed the following --------------------------
    Created the AppDbContext and registered the same as the In-Memory database in the Program.cs

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,3 +1,6 @@
+using PlatformService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+/*
+* Adding an In-Memory database.
+*/
+builder.Services.AddDbContext<AppDbContext>(opt =>
+ opt.UseInMemoryDatabase("InMem"));
 
 var app = builder.Build();
 
